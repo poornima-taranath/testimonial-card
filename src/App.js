d9317345ad91e233edcb19b20c96030e5c1df82f
Poornima <45766772+poornima-taranath@users.noreply.github.com>
2024-05-30 07:58:39 +0530

    added proper positioning and colors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <div className="card">
           <div className="heading">
             <img
