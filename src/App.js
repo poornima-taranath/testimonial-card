687b816e99c1c2190f2f9d7dec96071765028e1b
Poornima <45766772+poornima-taranath@users.noreply.github.com>
2024-05-30 11:15:00 +0530

    added varient colors for BG

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
           </div>
           <p>
             I've been searching for high-quaity abstract iamges for my design
-            projects and I'm thrilled to have found this platform.The variety
-            and depth of creativity are astounding.
+            projects and I'm thrilled to have found this platform. The variety
+            and depth of creativity are astounding!.
           </p>
         </div>
       </header>
